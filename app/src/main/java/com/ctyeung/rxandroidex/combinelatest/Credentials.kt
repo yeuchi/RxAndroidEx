d4e722c3e51677116315bdf9a51a4bde2b2b4496
Chi Yeung <yeun0007@umn.edu>
2020-07-04 07:16:36 -0500

    WIP CombineLast for credentials

--- app/src/main/java/com/ctyeung/rxandroidex/combinelatest/Credentials.kt
+++ app/src/main/java/com/ctyeung/rxandroidex/combinelatest/Credentials.kt
@@ -3,8 +3,7 @@ package com.ctyeung.rxandroidex.combinelatest
 import android.widget.EditText
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import java.util.concurrent.TimeUnit
+import io.reactivex.functions.BiFunction
 
 /*
  * https://medium.com/mindorks/android-form-validation-with-rxjava-3a8673886101
@@ -25,66 +24,30 @@ class Credentials {
 
         if(username!=null && password!=null) {
 
-            RxTextView.afterTextChangeEvents(username)
-                .skipInitialValue()
-                .debounce(400, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    // display message -- toast
+            val nameObservable: Observable<String> =
+                RxTextView.textChanges(username).skip(1)
+                    .map({str -> doTranform(str)})
+
+            val passwordObservable: Observable<String> =
+                RxTextView.textChanges(password).skip(1)
+                    .map({str -> doTranform(str)})
+
+            Observable.combineLatest(nameObservable,
+                passwordObservable,
+                BiFunction { observable1Times: Any, observable2Times: Any -> "Refreshed Observable1: $observable1Times refreshed Observable2: $observable2Times" }
+            )
+                .subscribe { item: String? ->
+                    println(
+                        item
+                    )
                 }
-//            val nameObservable: Observable<String> =
-//                RxTextView.textChanges(username).skip(1)
-//                    .map(object : Function<CharSequence?, String?>() {
-//                        @Throws(Exception::class)
-//                        fun apply(charSequence: CharSequence): String? {
-//                            return charSequence.toString()
-//                        }
-//                    })
-
-//            val passwordObservable: Observable<String> =
-//                RxTextView.textChanges(password).skip(1)
-//                    .map(object : Function<CharSequence?, String?>() {
-//                        @Throws(java.lang.Exception::class)
-//                        fun apply(charSequence: CharSequence): String? {
-//                            return charSequence.toString()
-//                        }
-//                    })
-
-//            observable = Observable.combineLatest(
-//                nameObservable,
-//                passwordObservable,
-//                object : BiFunction<String?, String?, Boolean?>() {
-//                    @Throws(java.lang.Exception::class)
-//                    fun apply(s: String?, s2: String?): Boolean? {
-//                        return isValidForm(s, s2)
-//                    }
-//                })
-//            observable.subscribe(object : DisposableObserver<Boolean?>() {
-//                override fun onNext(aBoolean: Boolean) {
-//                    updateButton(aBoolean)
-//                }
-//
-//                override fun onError(e: Throwable) {}
-//                override fun onComplete() {}
-//            })
         }
     }
 
-//    fun updateButton(valid: Boolean) {
-//        if (valid) btn_login.setEnabled(true)
-//    }
-//
-//    fun isValidForm(name: String, password: String): Boolean {
-//        val validName = !name.isEmpty()
-//        if (!validName) {
-//            this.username?.setError("Please enter valid name")
-//        }
-//        val validPass = !password.isEmpty() && password == AppPrefs.getPassword()
-//        if (!validPass) {
-//            this.password?.setError("Incorrect password")
-//        }
-//        return validName && validPass
-//    }
+    private fun doTranform(str: CharSequence): String? {
+        return null
+    }
+
 
     fun destroy() {
 
